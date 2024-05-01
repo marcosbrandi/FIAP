7e9da6f64a1fed5d428ae3370f05fd7fea587b8f
Marcos Brandi Torres <marcosbrandi@hotmail.com>
2024-05-01 17:03:50 -0300

    cleaning

--- TechChallengeFIAP.API/Program.cs
+++ TechChallengeFIAP.API/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using TechChallengeFIAP.API.EndPoints;
-using TechChallengeFIAP.Core.Entities;
 using TechChallengeFIAP.Infrastracture.Data;
 
 
@@ -19,6 +18,7 @@ if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
 app.UseSwagger();
 
+
 ContatosEndpoint.Map(app);
 
 
@@ -28,162 +28,3 @@ app.Run();
 
 
 
-
-
-/*
-app.MapGet("/", (context) =>
-{
-    return Results.Redirect("swagger/index.html");
-});
-*/
-
-/*
-app.MapGet("/", (context) =>
-{
-    return (Task)Results.Redirect("swagger/index.html");
-});
-
-
-const string baseUrl = @"/v1/contatos";
-var group = app.MapGroup(baseUrl);
-
-group.MapGet("/", async (FiapDbContext db) =>
-{
-    return await db.Contatos.ToListAsync();
-});
-
-group.MapGet("/{id}", async (int id, FiapDbContext db) =>
-{
-    return await db.Contatos.FindAsync(id) is Contato item ? Results.Ok(item) : Results.NotFound();
-});
-
-group.MapPost("", async (Contato contato, FiapDbContext db) =>
-{
-    db.Contatos.Add(contato);
-    await db.SaveChangesAsync();
-    return Results.Created($"{baseUrl}/{contato.Id}", contato);
-});
-
-group.MapPut("/{id}", async (int id, Contato contato, FiapDbContext db) =>
-{
-    var existItem = await db.Contatos.FindAsync(id);
-
-    if (existItem is null) return Results.NotFound();
-
-    existItem.Nome = contato.Nome;
-    existItem.Telefone = contato.Telefone;
-    existItem.Email = contato.Email;
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-group.MapDelete("/{id}", async (int id, FiapDbContext db) =>
-{
-    if (await db.Contatos.FindAsync(id) is Contato contato)
-    {
-        db.Contatos.Remove(contato);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
-*/
-
-/*
-
-app.MapGet(baseUrl, async (FiapDbContext db) =>
-    await db.Contatos.ToListAsync());
-
-app.MapGet(baseUrl+"/{id}", async (int id, FiapDbContext db) =>
-    await db.Contatos.FindAsync(id) is Contato item ? Results.Ok(item) : Results.NotFound());
-
-app.MapPost("/v1/contatos", async (Contato contato, FiapDbContext db) =>
-{
-    db.Contatos.Add(contato);
-    await db.SaveChangesAsync();
-    return Results.Created($"{baseUrl}/{contato.Id}", contato);
-});
-
-app.MapPut("/v1/contatos/{id}", async (int id, Contato contato, FiapDbContext db) =>
-{
-    var existItem = await db.Contatos.FindAsync(id);
-
-    if (existItem is null) return Results.NotFound();
-
-    existItem.Nome = contato.Nome;
-    existItem.Telefone = contato.Telefone;
-    existItem.Email = contato.Email;
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/v1/contatos/{id}", async (int id, FiapDbContext db) =>
-{
-    if (await db.Contatos.FindAsync(id) is Contato contato)
-    {
-        db.Contatos.Remove(contato);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-app.MapGet(baseUrl, ([FromServices] ContatoRepository items) =>
-{
-    var result = items.GetAllAsync();
-    return result != null ? Results.Ok(result) : Results.NotFound();
-}).Produces<List<Contato>>();
-
-app.MapGet("{baseUrl}/{id}", ([FromServices] ContatoRepository items, int id) =>
-{
-    var result = items.GetByIdAsync(id);
-    return result != null ? Results.Ok(result) : Results.NotFound();
-}).Produces<Contato>();
-
-app.MapPost("/v1/contatos", ([FromServices] ContatoRepository items, Contato item) =>
-{
-    items.Add(item);
-    //return Results.Created($"/v1/contatos/{item.Id}", item);
-    return Results.Created($"{baseUrl}/{item.Id}", item);
-});
-
-//app.MapPut("/v1/contatos/{id}", ([FromServices] ContatoRepository items, int id, Contato item) =>
-app.MapPut($"{{baseUrl}}/{{id}}", ([FromServices] ContatoRepository items, int id, Contato item) =>
-{
-    if (items.GetByIdAsync(id) == null)
-    {
-        return Results.NotFound();
-    }
-    items.Update(item);
-    return Results.Ok(item);
-});
-
-//app.MapDelete("/v1/contatos/{id}", ([FromServices] ContatoRepository items, int id) =>
-app.MapDelete("{baseUrl}/{id}", ([FromServices] ContatoRepository items, int id) =>
-{
-    var item = items.GetByIdAsync(id);
-    //if (items.GetByIdAsync(id) == null)
-    if (item == null)
-    {
-        return Results.NotFound();
-    }
-    items.Delete(item);
-    return Results.NoContent();
-});
-*/
