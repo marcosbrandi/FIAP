4649db0282b0a1f1ea290628a237b55ce58f2b01
Marcos Brandi Torres <marcosbrandi@hotmail.com>
2024-07-17 21:32:21 -0300

    Prometheus endpoints

---
+++ TechChallengeFIAP.API/Middleware/APIEndpoints.cs
@@ -0,0 +1,77 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using TechChallengeFIAP.Core.Entities;
+using TechChallengeFIAP.Core.Interfaces;
+
+namespace TechChallengeFIAP.API.Middleware;
+
+public static class APIEndpoints
+{
+    public static void Configure(WebApplication app)
+    {
+        const string baseUrl = @"/v1/contatos";
+
+        // Retorna um contato pelo DDD ou todos os contatos
+        app.MapGet("/Buscar/DDD", async (string? DDD, IContatoRepository repository) =>
+        {
+            var contatos = await repository.GetAllAsync(DDD);
+            return (contatos == null || contatos.Count() == 0) ?
+                Results.NotFound($"Nenhum Contato Encontrado {(DDD is null ? "" : $"com o DDD {DDD}")}") :
+                Results.Ok(contatos);
+        })
+        .WithMetadata(new SwaggerOperationAttribute("Retorna todos os contatos correspondentes ao DDD recebido como parâmetro. Caso não seja informado o DDD, retorna todos os contatos cadastrados"));
+
+        // Retorna um contato pelo Nome
+        app.MapGet($"/Buscar/Nome", async (string nome, IContatoRepository repository)
+            => await repository.GetByNameAsync(nome) is Contato item ? Results.Ok(item) : Results.NotFound($"Contato {nome} não localizado."))
+            .WithMetadata(new SwaggerOperationAttribute("Retorna um contato existente, passanto o Nome como parâmetro"));
+
+        // Retorna um contato pelo ID
+        app.MapGet("/Buscar/Id", async (int id, IContatoRepository repository)
+            => await repository.FindAsync(id) is Contato item ? Results.Ok(item) : Results.NotFound($"Contato ID {id} não localizado.")).
+               WithMetadata(new SwaggerOperationAttribute($@"Retorna um contato passando Id de registro como parâmetro", "description001"));
+
+        // Retorna informações sobre o DDD
+        app.MapGet("/Buscar/UfPorDDD", async (string DDD, IContatoRepository repository) =>
+        {
+            var contatos = await repository.GetAllAsync(DDD);
+            return (contatos == null || contatos.Count() == 0) ?
+                Results.NotFound($"Contatos com o DDD: {DDD} não encontrado.") :
+                Results.Ok(contatos);
+
+        }).WithMetadata(new SwaggerOperationAttribute("Retorna o estado correspondente ao DDD recebido como parâmetro"));
+
+        // Adiciona um novo contato
+        app.MapPost("Inserir/Contato", async (Contato contato, IContatoRepository repository) =>
+        {
+            await repository.AddAsync(contato);
+            return Results.Created($"{baseUrl}/{contato.Id}", contato);
+        }).WithMetadata(new SwaggerOperationAttribute($"Cria um novo contato, os parâmetros devem corresponder ao body do json, há validações para Id e E-mail repetido"));
+
+        // Atualiza um contato
+        app.MapPut("Atualizar/Contato", async (int id, Contato contato, IContatoRepository repository) =>
+        {
+            Contato currentContato = await repository.FindAsync(id);
+            if (currentContato != null)
+            {
+                await repository.UpdateAsync(currentContato, contato);
+                return Results.Ok($"Registro(s) atualizado(s) com sucesso!");
+            }
+            return Results.NotFound($"Contato ID {id} não localizado.");
+
+        }).WithMetadata(new SwaggerOperationAttribute("Atualiza um contato existente"));
+
+        // Deleta um contato
+        app.MapDelete("Deletar/Contato", async (int id, IContatoRepository repository) =>
+        {
+            if (await repository.FindAsync(id) is Contato contato)
+            {
+                await repository.DeleteAsync(contato);
+                return Results.Ok($"Registro excluído com sucesso!");
+            }
+            return Results.NotFound();
+        }).WithMetadata(new SwaggerOperationAttribute("Deleta um contato correspondente ao Id recebido como parâmetro"));
+
+    }
+
+}
+
---
+++ TechChallengeFIAP.API/Middleware/PrometheusEndpoints.cs
@@ -0,0 +1,75 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using TechChallengeFIAP.Core.Entities;
+using TechChallengeFIAP.Core.Interfaces;
+using Prometheus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace TechChallengeFIAP.API.Middleware;
+
+public static class PrometheusEndpoints
+{
+    public static void Configure(WebApplication app)
+    {
+        Counter _counter = Metrics.CreateCounter("TestMetricCounter", "will be incremented and published as metrics");
+        //Histogram _histogram = Metrics.CreateHistogram("TestMetricHistogram", "Will observe a value and publish it as Histogram");
+        //Gauge _gauge = Metrics.CreateGauge("TestMetricGauge", "Will observe a value and publish it as Gauge");
+        //Summary _summary = Metrics.CreateSummary("TestMetricSummary", "Will observe a value and publish it as Summary");
+
+        var counter = Metrics.CreateCounter("TestMetricCounter", "Counts requests to endpoints", 
+            new CounterConfiguration
+            {
+                LabelNames = new[] { "method", "endpoint" }
+            });
+
+        app.Use((context, next) =>
+        {
+            counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+            return next();
+        });
+
+        app.UseHealthChecks("/hc", new HealthCheckOptions
+        {
+            Predicate = _ => true
+        });
+
+        /*
+        app.MapPost("/IncrementCounter", (int inc) =>
+        {
+            _counter.Inc(inc);
+            _counter.Publish();
+        })
+        .WithMetadata(new SwaggerOperationAttribute("IncrementCounter"));
+
+        app.MapPost("/IncrementHistogram", (int inc) =>
+        {
+            _histogram.Observe(inc);
+            _histogram.Publish();
+        })
+        .WithMetadata(new SwaggerOperationAttribute("IncrementHistogram"));
+
+        app.MapPost("/IncrementGauge", (int inc) =>
+        {
+            _gauge.Inc(inc);
+            _gauge.Publish();
+        })
+        .WithMetadata(new SwaggerOperationAttribute("IncrementGauge"));
+
+        app.MapPost("/DecrementGauge", (int inc) =>
+        {
+            _gauge.Dec(inc);
+            _gauge.Publish();
+        })
+        .WithMetadata(new SwaggerOperationAttribute("DecrementGauge"));
+
+        app.MapPost("/IncrementSummary", (int inc) =>
+        {
+            _summary.Observe(inc);
+            _summary.Publish();
+        })
+        .WithMetadata(new SwaggerOperationAttribute("IncrementSummary"));
+        */
+    }
+
+}
+
