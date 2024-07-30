8d16c86c0659cced137b6159df5845f98d2644f0
Gustavo Amaral Pereira <gustavo-amaral@hotmail.com.br>
2024-07-30 18:45:39 -0300

    push nova versão

--- TechChallengeFIAP.API/Program.cs
+++ TechChallengeFIAP.API/Program.cs
@@ -40,10 +40,12 @@ ServiceInterfaces.Add(builder.Services);
 
 var app = builder.Build();
 
-PrometheusEndpoints.Configure(app);
-
 app.UsePrometheusServer();
-app.UseHttpMetrics();
+
+app.UseHttpMetrics(options =>
+{
+    options.AddCustomLabel("host", context => context.Request.Host.Host);
+});
 
 if (app.Environment.IsDevelopment())
 {
@@ -59,71 +61,30 @@ app.UseSwaggerUI(c => {
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<FiapDbContext>();
-    dbContext.Database.Migrate();
 }
 
-// declara endpoints
-const string baseUrl = @"/v1/contatos";
+// declare endpoints
+APIEndpoints.Configure(app);
 
-// Retorna um contato pelo DDD ou todos os contatos
-app.MapGet("/Buscar/DDD", async (string? DDD, IContatoRepository repository) =>
-{
-    var contatos = await repository.GetAllAsync(DDD);
-    return (contatos == null || contatos.Count() == 0) ?
-        Results.NotFound($"Nenhum Contato Encontrado {(DDD is null ? "" : $"com o DDD {DDD}")}") :
-        Results.Ok(contatos);
-})
-.WithMetadata(new SwaggerOperationAttribute("Retorna todos os contatos correspondentes ao DDD recebido como parâmetro. Caso não seja informado o DDD, retorna todos os contatos cadastrados"));
+// declare Prometheus endpoints
+PrometheusEndpoints.Configure(app);
 
-// Retorna um contato pelo Nome
-app.MapGet($"/Buscar/Nome", async (string nome, IContatoRepository repository)
-    => await repository.GetByNameAsync(nome) is Contato item ? Results.Ok(item) : Results.NotFound($"Contato {nome} não localizado."))
-    .WithMetadata(new SwaggerOperationAttribute("Retorna um contato existente, passanto o Nome como parâmetro"));
+Counter _counter = Metrics.CreateCounter("TestMetricCounter", "will be incremented and published as metrics");
+//Histogram _histogram = Metrics.CreateHistogram("TestMetricHistogram", "Will observe a value and publish it as Histogram");
+//Gauge _gauge = Metrics.CreateGauge("TestMetricGauge", "Will observe a value and publish it as Gauge");
+//Summary _summary = Metrics.CreateSummary("TestMetricSummary", "Will observe a value and publish it as Summary");
+app.UseMetricServer();
 
-// Retorna um contato pelo ID
-app.MapGet("/Buscar/Id", async (int id, IContatoRepository repository)
-    => await repository.FindAsync(id) is Contato item ? Results.Ok(item) : Results.NotFound($"Contato ID {id} não localizado.")).
-       WithMetadata(new SwaggerOperationAttribute($@"Retorna um contato passando Id de registro como parâmetro", "description001"));
-
-// Retorna informações sobre o DDD
-app.MapGet("/Buscar/UfPorDDD", async (string DDD, IContatoRepository repository) =>
-{
-    var contatos = await repository.GetAllAsync(DDD);
-    return (contatos == null || contatos.Count() == 0) ?
-        Results.NotFound($"Contatos com o DDD: {DDD} não encontrado.") :
-        Results.Ok(contatos);
-
-}).WithMetadata(new SwaggerOperationAttribute("Retorna o estado correspondente ao DDD recebido como parâmetro"));
-
-// Adiciona um novo contato
-app.MapPost("Inserir/Contato", async (Contato contato, IContatoRepository repository) =>
-{
-    await repository.AddAsync(contato);
-    return Results.Created($"{baseUrl}/{contato.Id}", contato);
-}).WithMetadata(new SwaggerOperationAttribute($"Cria um novo contato, os parâmetros devem corresponder ao body do json, há validações para Id e E-mail repetido"));
-
-// Atualiza um contato
-app.MapPut("Atualizar/Contato", async (int id, Contato contato, IContatoRepository repository) =>
-{
-    Contato currentContato = await repository.FindAsync(id);
-    if (currentContato != null)
+var counter = Metrics.CreateCounter("TestMetricCounter", "Counts requests to endpoints",
+    new CounterConfiguration
     {
-        await repository.UpdateAsync(currentContato, contato);
-        return Results.Ok($"Registro(s) atualizado(s) com sucesso!");
-    }
-    return Results.NotFound($"Contato ID {id} não localizado.");
+        LabelNames = new[] { "method", "endpoint" }
+    });
 
-}).WithMetadata(new SwaggerOperationAttribute("Atualiza um contato existente"));
-
-// Deleta um contato
-app.MapDelete("Deletar/Contato", async (int id, IContatoRepository repository) =>
+app.Use((context, next) =>
 {
-    if (await repository.FindAsync(id) is Contato contato)
-    {
-        await repository.DeleteAsync(contato);
-        return Results.Ok($"Registro excluído com sucesso!");
-    }
-    return Results.NotFound();
-}).WithMetadata(new SwaggerOperationAttribute("Deleta um contato correspondente ao Id recebido como parâmetro"));
+    counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+    return next();
+});
 
 app.Run();
