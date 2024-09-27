a1a5f656da0c23ec69115003cd72ae5d0a56f0b8
Marcos Brandi Torres <marcosbrandi@hotmail.com>
2024-09-27 20:22:40 -0300

    add prometheus endpoint

--- TechChallenge.API/Program.cs
+++ TechChallenge.API/Program.cs
@@ -4,6 +4,11 @@ using MediatR;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
 using TechChallenge.API.Endpoints;
+using Prometheus;
+using Prometheus.Client.AspNetCore;
+using Prometheus.Client.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using TechChallengeFIAP.API.Middleware;
 
 namespace TechChallenge.Consumer
 {
@@ -16,6 +21,12 @@ namespace TechChallenge.Consumer
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services
+                .AddHealthChecks()
+                .AddUrlGroup(new Uri("https://google.com"), "google", HealthStatus.Unhealthy)
+                .AddUrlGroup(new Uri("https://invalidurl"), "invalidurl", HealthStatus.Degraded);
+            builder.Services.AddPrometheusHealthCheckPublisher();
 
             builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Contatos API", Description = "Cadastro de Contatos", Version = "v1" }); });
 
@@ -32,16 +43,21 @@ namespace TechChallenge.Consumer
             if (app.Environment.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
             app.UseSwagger();
 
+            //These metrics are called to use prometheus
+            app.UseMetricServer();
+            app.UseHttpMetrics();
+            app.UsePrometheusServer();
+
             //ContatoEndpoints.Map(app);
             ContatoEndpoints.Map1(app);
             BuscarContatos.AddRoutes(app);
+            PrometheusEndpoints.Configure(app);
 
             app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contatos API V1"); });
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
